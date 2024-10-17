00946d5ffbc76bc846cfd58b8444e4b4c8214cf7
dmitry <dubrovsky.ds@delta.ru>
2024-10-17 14:05:10 +0300

    правка админки

--- cat/admin.py
+++ cat/admin.py
@@ -5,11 +5,14 @@ from cat.models import CatImage
 
 
 class CatImageAdmin(admin.ModelAdmin):
-    list_display = ('image', 'is_public')
-    readonly_fields = ('image',)
+    list_display = ('image_thumbnail', 'is_public')
+    list_editable = ('is_public',)
 
     def image_thumbnail(self, obj):
-        return mark_safe('<img src="%s" width="100" height="100" />' % (obj.image.url))
+        if obj.image:
+            return mark_safe('<img src="%s" width="100" height="100" />' % (obj.image.url))
+        else:
+            return "Изображение не загружено"
 
     image_thumbnail.short_description = 'Миниатюра'
     image_thumbnail.allow_tags = True
